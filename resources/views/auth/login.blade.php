b6bb4a19c3ee5df970ceaab9a31e6e9c2bb0e045
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-10-05 14:46:16 -0500

    Vista de login completa y lista

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -71,3 +71,113 @@
     </div>
 </div>
 @endsection
+ --}}
+
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Iniciar Sesión</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{ asset('assets_login/images/icons/favicon.ico') }}" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/vendor/animate/animate.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/vendor/animsition/css/animsition.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/vendor/select2/select2.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/vendor/daterangepicker/daterangepicker.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets_login/css/main.css') }}">
+    <!--===============================================================================================-->
+</head>
+
+<body style="background-color: rgb(238,238,238);">
+
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <span class="login100-form-title p-b-43">
+                        Inicia Sesión
+                    </span>
+
+
+                    <div class="wrap-input100 validate-input" data-validate="Correo válido: ejemplo@abc.xyz">
+                        <input type="text" class="input100" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <span class="focus-input100"></span>
+                        <span class="label-input100">Correo Electrónico</span>
+                    </div>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+
+
+                    <div class="wrap-input100 validate-input" data-validate="Contraseña requerida">
+                        <input type="password" class="input100 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <span class="focus-input100"></span>
+                        <span class="label-input100">Contraseña</span>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    {{-- Mensaje de error para la validación del login --}}
+                    @error('email') <span style="color: red">{{ $message }}</span> @enderror
+                    {{-- Mensaje de error para la validación del login --}}
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            Entrar
+                        </button>
+                    </div>
+
+                </form>
+
+                <div class="login100-more" style="background-image: url('assets_login/images/bg-01.jpg');">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets_login/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets_login/vendor/animsition/js/animsition.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets_login/vendor/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ asset('assets_login/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets_login/vendor/select2/select2.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets_login/vendor/daterangepicker/moment.min.js') }}"></script>
+    <script src="{{ asset('assets_login/vendor/daterangepicker/daterangepicker.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets_login/vendor/countdowntime/countdowntime.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('assets_login/js/main.js') }}"></script>
+
+</body>
+
+</html>
