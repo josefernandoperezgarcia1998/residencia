a51e40eb72c5c71303792ab3c9b7dbcd3ba1b15a
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-10-06 13:45:32 -0500

    Modelo Paciente con carga masiva

---
+++ app/Models/Paciente.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Paciente extends Model
+{
+    use HasFactory;
+
+    /* Carga masiva de las columnas en la tabla Paciente */
+    protected $fillable = [
+        'nombre',
+        'a_paterno',
+        'a_materno',
+        'edad',
+        'estado_civil',
+        'telefono_celular',
+        'email',
+        'peso',
+        'talla',
+        'sistólica',
+        'diastólica',
+    ];
+}
