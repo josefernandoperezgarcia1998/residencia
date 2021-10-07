8751782e50e4a06e1185fe64086eed98057a5123
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-10-07 18:25:59 -0500

    Sin modificación

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Paciente;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -28,4 +30,5 @@ class HomeController extends Controller
 
         return view('dashboard', compact('pacientes'));
     }
+    
 }
