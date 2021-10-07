fc4510d5e95c0252f6b6b1604d0845729e0a5914
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-10-07 18:22:13 -0500

    Vista index para el historial médico

---
+++ resources/views/historial_medico/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.general')
+
+@section('nombre_de_la_pagina')
+Historial Médico
+@endsection
+
+@section('contenido_de_la_pagina')
+<div class="content">
+    @if (session('success'))
+    <div class="alert alert-success">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <i class="material-icons">close</i>
+        </button>
+        <span>{{ session('success')}}</span>
+    </div>
+
+    @endif
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="{{ route('historial_medico.create') }}" class="btn btn-info btn-sm">Nueva cita</a>
+                <div class="card">
+                    <div class="card-header card-header-primary">
+                        <h4 class="card-title ">Historial Médico General</h4>
+                        <p class="card-category">Listado de citas médicas registradas en el sistema</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead class="text-primary">
+                                    <th>ID</th>
+                                    <th>Nombre del paciente</th>
+                                    <th>Fecha de la cita</th>
+                                    <th>Acciones</th>
+                                </thead>
+                                <tbody>
+                                    @forelse ($citas as $cita)
+                                        <tr>
+                                            <td>{{$cita->id}}</td>
+                                            <td>{{$cita->paciente->nombre}}</td>
+                                            <td>{{$cita->created_at}}</td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td>No se encuentra ningun registro de citas en el sistema...</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endsection
