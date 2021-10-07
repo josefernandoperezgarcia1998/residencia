70dc0b3342e0702de5a0d50c6248cb67bde77761
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-10-07 18:24:04 -0500

    Se creó la migración para la tabla de Citas

---
+++ database/migrations/2021_10_06_232746_create_citas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();
+            $table->string('medicamento')->nullable();
+            $table->string('padecimiento')->nullable();
+            $table->string('terapia')->nullable();
+            $table->string('terapia_otro')->nullable();
+            $table->enum('ozono', ['si','no'])->nullable();
+            $table->enum('acupuntura', ['si','no'])->nullable();
+            $table->enum('limpieza_oido', ['si','no'])->nullable();
+            $table->enum('mejoria', ['si','no'])->nullable();
+            $table->string('mejoria_descripcion')->nullable();
+            $table->string('sintoma_inquietud')->nullable();
+            $table->string('hora_cita')->nullable();
+            $table->foreignId('paciente_id')->references('id')->on('pacientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+}
