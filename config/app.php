dc5a0e8561ca74560b4429678af55b11f04b1a07
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-10-11 03:22:26 -0500

    Agregando facade y alias para dompdf

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -232,7 +233,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
-
+        'PDF' => Barryvdh\DomPDF\Facade::class,
+        
     ],
 
 ];
