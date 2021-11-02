d7bb59eb0689d34aefef31e99ade12afc59134d3
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-11-02 05:07:03 -0600

    Migracion, factory, seeder y modelo de la tabla  Contacto

---
+++ database/migrations/2021_11_02_031456_create_contactos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_completo');
+            $table->string('email');
+            $table->string('numero');
+            $table->string('mensaje');
+            $table->tinyInteger('concesionado')->nullable();
+            $table->string('hora_cita')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contactos');
+    }
+}
