7e832d80e37920bf27161a58f5d8410c5d289c53
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2021-11-02 05:12:19 -0600

    Gate para poder revisar un mensaje

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\Contacto;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //Gate para cuando un contacto esté concesionado
+        Gate::define('revisar', function ($mensaje) {
+            return $mensaje->concesionado != 0;
+        });
     }
 }
